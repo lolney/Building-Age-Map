958c0668c992f9e5a3cc225604c003729faeacef
Luke Olney <LukeOlney2016@u.northwestern.edu>
2017-06-16 21:27:07 -0700

    delete geoson file (+2 squashed commits) Squashed commits: [2270c15] Add vector layer [3af9eea] Added example building footprint

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibG9sbmV5IiwiYSI6Il9rMGRGa2cifQ.xtEz-bJjWVzaJB
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-    center: [-74.50, 40], // starting position
-    zoom: 9 // starting zoom
+    center: [-122.415887,37.735618], // starting position
+    zoom: 17 // starting zoom
 });
+
+map.on("load", function(){
+
+    map.addLayer({
+        "id": "building-footprints-vector",
+        "type": "fill",
+        "source": {
+            type: 'vector',
+            url: 'mapbox://lolney.61zxsbxz'
+        },
+        "source-layer": "building_footprintsgeojson",
+        "paint": {
+            "fill-color": "#666666",
+            "fill-opacity": 0.7
+        }
+    });
+
+})
