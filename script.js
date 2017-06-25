3990e1269e332bfffd056b2996018f460724d2d7
Luke Olney <LukeOlney2016@u.northwestern.edu>
2017-06-24 18:00:53 -0700

    filter is interactive

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-var $ = window.jQuery = require("jquery")
-require("jquery-ui")
+/*var $ = window.jQuery = require("jquery")
+require("jquery-ui")*/
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibG9sbmV5IiwiYSI6Il9rMGRGa2cifQ.xtEz-bJjWVzaJBUK2sWBsA';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-    center: [-122.415887,37.735618], // starting position
-    zoom: 17 // starting zoom
+    style: 'mapbox://styles/lolney/cj40toyp15dsn2ss1t7o1wocs', //stylesheet location
+    center: [-122.435887,37.765618], // starting position
+    zoom: 12 // starting zoom
 });
+
+var ageFilter = function(l,h){
+	var newfilter = ['all', ['>', 'Year Property Built', l], ['<', 'Year Property Built', h]];
+	map.setFilter('building-footprints-vector', newfilter);
+}
+
+var ageFilterRange = function(range){
+	ageFilter(range['l'].toString(), range['h'].toString());
+}
+
+var range = {'l':1860, 'h':1900};
 
 map.on("load", function(){
 
@@ -16,26 +27,34 @@ map.on("load", function(){
         "type": "fill",
         "source": {
             type: 'vector',
-            url: 'mapbox://lolney.61zxsbxz'
+            url: 'mapbox://lolney.bqk3icqv'
         },
-        "source-layer": "building_footprintsgeojson",
+        "source-layer": "joinedLotsgeojson",
         "paint": {
             "fill-color": "#666666",
             "fill-opacity": 0.7
         }
     });
 
-})
-
-$( "#slider-range" ).slider({
+    $( "#slider-range" ).slider({
 	  range: true,
-	  min: 0,
-	  max: 500,
-	  values: [ 75, 300 ],
+	  min: 1850,
+	  max: 2000,
+	  values: [ range['l'], range['h'] ],
 	  slide: function( event, ui ) {
-	    $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+	    $( "#amount" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+	    range['l'] = ui.values[0];
+	    range['h'] = ui.values[1];
+	    ageFilterRange(range);
 	  }
 	});
 
-$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+	$( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ) +
+	  " - " + $( "#slider-range" ).slider( "values", 1 ) );
+
+	ageFilterRange(range)
+
+})
+
+
+
